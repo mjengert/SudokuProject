5388108416d5c6ed4f6e435803fa828d83a3824a
mjengert <m.engert@ufl.edu>
2023-04-08 16:13:28 -0500

    updated button functionality

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,8 @@ import pygame, sys
 from constants import *
 from board import Board
 from cell import Cell
+
+
 # We can discuss font choice and background/color scheme together
 # Ones in place hold that spot for testing before we decide
 
@@ -35,7 +37,6 @@ def easy_button(color):
     pygame.draw.rect(screen, color, [337.5 - 70, 420, 140, 40])
     screen.blit(button_easy, (337.5 - 30, 420))
     pygame.display.update()
-
 
 
 # draws medium button
@@ -89,85 +90,92 @@ def game_button_outlines(color):
     pygame.draw.rect(screen, color, [337.5 - 75, 695, 150, 50])
     pygame.draw.rect(screen, color, [506.25 - 75, 695, 150, 50])
 
-
-# initializes the menu screen and draws difficulty buttons
-pygame.init()
-pygame.font.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# can change background; placeholder
-background_im = pygame.image.load("sudokuim.png")
-game1 = Board(WIDTH, HEIGHT, screen, 'n/a')
-screen.blit(background_im, (0, 0))
-start_menu()
-start_button_outlines((64, 94, 0))
-easy_button((51, 50, 50))
-med_button((51, 50, 50))
-hard_button((51, 50, 50))
-game_on = False
-
-while not game_on:
-    # loop looks at all events within game
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        # checks mouse position and determines if it is clicked
-        # currently only exits the game for test purposes; will provide functionality after
-        mouse_pos = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+# starts program and draws menu and buttons
+game_on = True
+while game_on:
+    pygame.init()
+    pygame.font.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    # can change background; placeholder
+    background_im = pygame.image.load("sudokuim.png")
+    game1 = Board(WIDTH, HEIGHT, screen, 'n/a')
+    screen.blit(background_im, (0, 0))
+    start_menu()
+    start_button_outlines((64, 94, 0))
+    easy_button((51, 50, 50))
+    med_button((51, 50, 50))
+    hard_button((51, 50, 50))
+    # initializes the menu screen
+    menu = True
+    while menu:
+        # loop looks at all events within game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            # checks mouse position and determines if it is clicked
+            # currently only exits the game for test purposes; will provide functionality after
+            mouse_pos = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 420 <= mouse_pos[1] <= 420 + 40:
+                    menu = False
+                if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 500 <= mouse_pos[1] <= 500 + 40:
+                    menu = False
+                if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 580 <= mouse_pos[1] <= 580 + 40:
+                    menu = False
+            # changes the color of the button if it is being hovered over
             if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 420 <= mouse_pos[1] <= 420 + 40:
-                game_on = True
-            if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 500 <= mouse_pos[1] <= 500 + 40:
-                game_on = True
-            if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 580 <= mouse_pos[1] <= 580 + 40:
-                game_on = True
-        # changes the color of the button if it is being hovered over
-        if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 420 <= mouse_pos[1] <= 420 + 40:
-            easy_button((130, 127, 127))
-        elif 267.5 <= mouse_pos[0] <= 337.5 + 70 and 500 <= mouse_pos[1] <= 500 + 40:
-            med_button((130, 127, 127))
-        elif 267.5 <= mouse_pos[0] <= 337.5 + 70 and 580 <= mouse_pos[1] <= 580 + 40:
-            hard_button((130, 127, 127))
-        # if button isn't being hovered over, it returns back to original color
-        else:
-            easy_button((51, 50, 50))
-            med_button((51, 50, 50))
-            hard_button((51, 50, 50))
-    pygame.display.update()
+                easy_button((130, 127, 127))
+            elif 267.5 <= mouse_pos[0] <= 337.5 + 70 and 500 <= mouse_pos[1] <= 500 + 40:
+                med_button((130, 127, 127))
+            elif 267.5 <= mouse_pos[0] <= 337.5 + 70 and 580 <= mouse_pos[1] <= 580 + 40:
+                hard_button((130, 127, 127))
+            # if button isn't being hovered over, it returns back to original color
+            else:
+                easy_button((51, 50, 50))
+                med_button((51, 50, 50))
+                hard_button((51, 50, 50))
+        pygame.display.update()
 
-screen.fill(BG_COLOR)
-game1.draw(screen)
-game_button_outlines((64, 94, 0))
-exit_button((51, 50, 50))
-restart_button((51, 50, 50))
-reset_button((51, 50, 50))
-game_start = True
-while game_start:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        mouse_pos = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+    # when difficulty is chosen, second while loop begins and sudoku board and buttons is printed
+    screen.fill(BG_COLOR)
+    game1.draw(screen)
+    game_button_outlines((64, 94, 0))
+    exit_button((51, 50, 50))
+    restart_button((51, 50, 50))
+    reset_button((51, 50, 50))
+    # begins game
+    game_start = True
+    while game_start:
+        for event in pygame.event.get():
+            # exits entire game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            mouse_pos = pygame.mouse.get_pos()
+            # determines where and if mouse is clicked on each button
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # not done
+                if 98.75 <= mouse_pos[0] <= 168.75 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
+                    pass
+                # brings user back to start menu
+                if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
+                    game_start = False
+                # allows user to exit the game
+                if 436.25 <= mouse_pos[0] <= 506.25 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
+                    pygame.quit()
+                    sys.exit()
+            # changes the color of the button if it is being hovered over
             if 98.75 <= mouse_pos[0] <= 168.75 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
-                pygame.quit()
-                sys.exit()
-            if 267.5 <= mouse_pos[0] <= 337.5 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
-                pygame.quit()
-                sys.exit()
-            if 436.25 <= mouse_pos[0] <= 506.25 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
-                pygame.quit()
-                sys.exit()
-        # changes the color of the button if it is being hovered over
-        if 98.75 <= mouse_pos[0] <= 168.75 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
-            reset_button((130, 127, 127))
-        elif 267.5 <= mouse_pos[0] <= 337.5 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
-            restart_button((130, 127, 127))
-        elif 436.25 <= mouse_pos[0] <= 506.25 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
-            exit_button((130, 127, 127))
-        # if button isn't being hovered over, it returns back to original color
-        else:
-            reset_button((51, 50, 50))
-            restart_button((51, 50, 50))
-            exit_button((51, 50, 50))
-    pygame.display.update()
+                reset_button((130, 127, 127))
+            elif 267.5 <= mouse_pos[0] <= 337.5 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
+                restart_button((130, 127, 127))
+            elif 436.25 <= mouse_pos[0] <= 506.25 + 70 and 700 <= mouse_pos[1] <= 700 + 40:
+                exit_button((130, 127, 127))
+            # if button isn't being hovered over, it returns back to original color
+            else:
+                reset_button((51, 50, 50))
+                restart_button((51, 50, 50))
+                exit_button((51, 50, 50))
+        pygame.display.update()
+
